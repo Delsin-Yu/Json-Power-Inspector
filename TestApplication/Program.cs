8e74009f6bcdb12709ab245334f82926d50efc29
Gaoyao Massimo Hu <ghzero97@gmail.com>
2024-05-07 17:01:25 +0100

    feat: Add complex model classes and enums

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using JsonPowerInspector.Template;
 
 var definition = TemplateSerializer.CollectDefinition(typeof(MyComplexModel), "Test Model");
@@ -11,4 +12,95 @@ Console.WriteLine(definition);
 public class MyComplexModel
 {
     // Create your ultra complex model here
+    public List<NationModel> Nations;
+    public List<CharacterModel> Characters;
+    public List<LocationModel> Locations;
+    public List<WeaponModel> Weapons;
+    public List<ItemModel> Items;
+}
+
+public enum Alignment
+{
+    Good,
+    Neutral,
+    Evil
+}
+
+public enum Gender
+{
+    Male,
+    Female,
+    Other
+}
+public class NationModel
+{
+    public string Name;
+    public List<CharacterModel> Characters;
+    public List<LocationModel> Locations;
+}
+public class CharacterModel
+{
+    public string Name;
+    public int Age;
+    public int Level;
+    public int Health;
+    public int Mana;
+    public Dictionary<string,ItemModel> Inventory;
+    public List<CharacterModel> Family;
+    public DateTime BirthDate;
+    public RaceModel Race;
+    public Alignment Alignment;
+    public List<WeaponModel>  Weapons;
+    public Gender Gender;
+    public string Description;
+    public string Background;
+    public List<HistoryModel> Histories;
+    public bool IsAlive;
+    public ReligionModel Religion;
+    
+}
+public class ReligionModel
+{
+    public string Name;
+    public string Description;
+    public List<CharacterModel> Characters;
+    public List<LocationModel> Locations;
+}
+public class HistoryModel
+{
+    public string Name;
+    public string Description;
+    public List<CharacterModel> Characters;
+    public List<LocationModel> Locations;
+}
+public class LocationModel
+{
+    public string Name;
+    public string Description;
+    public List<CharacterModel> Characters;
+    public List<LocationModel> SubLocations;
+}
+public class WeaponModel
+{
+    public string Name;
+    public int Level;
+    public int Durability;
+    public int Weight;
+    public int Value;
+    public int Damage;
+    public int Range;
+}
+public class ItemModel
+{
+    public string Name;
+    public int Level;
+    public int Durability;
+    public int Weight;
+    public int Value;
+}
+public class RaceModel
+{
+    public string Name;
+    public RaceModel ParentRace;
+    public List<RaceModel> SubRaces;
 }
