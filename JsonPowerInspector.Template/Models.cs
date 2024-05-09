6c7d354658b4ccf5243b47b2fc34cbb698ac03f3
DE-YU <delsin_yu@qq.com>
2024-05-09 21:45:51 +0800

    Code cleaning

--- JsonPowerInspector.Template/Models.cs
+++ JsonPowerInspector.Template/Models.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Linq;
 using System.Text;
-using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace JsonPowerInspector.Template;
--- JsonPowerInspector/Scripts/Utils.cs
+++ JsonPowerInspector/Scripts/Utils.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
 using System.Text.Json.Nodes;
-using Godot;
 using JsonPowerInspector.Template;
 
 namespace JsonPowerInspector;
