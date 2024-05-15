b7fd1fefef77eaf4235b1e3213b405dd12f67c62
DE-YU <delsin_yu@qq.com>
2024-05-15 17:15:09 +0800

    Make label clickable

---
+++ JsonPowerInspector/Scripts/propergate_to_button.gd
@@ -0,0 +1,11 @@
+extends Label
+
+@export var button : Button;
+
+func _ready():
+	mouse_filter = MOUSE_FILTER_STOP;
+
+func _gui_input(event):
+	var mouse_button_event := event as InputEventMouseButton;
+	if(mouse_button_event == null || mouse_button_event.button_index != 1 || !mouse_button_event.is_pressed()) : return;
+	button.button_pressed = !button.button_pressed;
