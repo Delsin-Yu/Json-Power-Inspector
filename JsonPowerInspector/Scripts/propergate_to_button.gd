c21286ef2390dc7197f520ed9efbd4ef30eeb7ee
DE-YU <delsin_yu@qq.com>
2024-05-15 17:20:55 +0800

    Formating

--- JsonPowerInspector/Scripts/propergate_to_button.gd
+++ JsonPowerInspector/Scripts/propergate_to_button.gd
@@ -1,4 +1,4 @@
-extends Label
+extends Label;
 
 @export var button : Button;
 
@@ -6,6 +6,14 @@ func _ready():
 	mouse_filter = MOUSE_FILTER_STOP;
 
 func _gui_input(event):
+	
 	var mouse_button_event := event as InputEventMouseButton;
-	if(mouse_button_event == null || mouse_button_event.button_index != 1 || !mouse_button_event.is_pressed()) : return;
+	
+	if (
+		mouse_button_event == null ||
+		mouse_button_event.button_index != 1 ||
+		!mouse_button_event.is_pressed()
+	) : return;
+	
 	button.button_pressed = !button.button_pressed;
+
